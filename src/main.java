883acdc13b7de22f3d039a50db7a3ea6d7e782cd
aryaparasharmrt <66545234+aryaparasharmrt@users.noreply.github.com>
2023-11-01 10:23:02 +0530

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+//root function of Java Project
+public static void main(Strings []args) {
+
+  // Taking User Input through Scanner
+  Scanner sc = new Scanner(System.in);
+  int num1 = sc.nextInt();
+  int num2 = sc.nextInt();
+
+  int result = num1 + num2;
+  System.out.print(result);
+}  
